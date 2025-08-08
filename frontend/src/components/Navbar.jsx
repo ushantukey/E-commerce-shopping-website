99aac4693d9c0be2211c73d8ed34615f44f7d761
ushantukey <ushantukey16@gmail.com>
2025-08-08 21:19:04 +0530

    create my order and login page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
   <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
 
   <div className='relative group'>
-    <img src={assets.profile_icon} className='w-5 cursor-pointer' alt='' />
+    <Link to='/login' > <img src={assets.profile_icon} className='w-5 cursor-pointer' alt='' /> </Link>
 
     <div className='hidden group-hover:block absolute right-0 top-full pt-2 z-10'>
       <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded shadow'>
