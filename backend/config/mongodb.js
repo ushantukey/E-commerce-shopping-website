3bbcba5d17228c9c390c545f10623fd08f495555
ushantukey <ushantukey16@gmail.com>
2025-08-11 00:24:14 +0530

    create the backend

---
+++ backend/config/mongodb.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("DB Connected");
+    });
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Connect to MongoDB
+connectDB();
+connectCloudinary();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// API Endpoints
+app.get('/', (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => console.log('Server started on PORT : ' + port));
